7aa576029e8a3ccb6dd5946a7db147609d99b90e
trevorspecht <trevor.specht@mapbox.com>
2018-03-14 14:37:12 -0400

    testing forEach

--- geocoding.js
+++ geocoding.js
@@ -1,15 +1,36 @@
 
 const request = require('request');
 
-// request('http://api.mapbox.com/geocoding/v5/mapbox.places/Baltimore.json?access_token=pk.eyJ1IjoidHJldm9yc3BlY2h0IiwiYSI6ImNqNDdjZ2hvNjAxYWkzMmp0YW90bWhsZmwifQ.0d4-iKYzF8sy7YFpzVOKUA', function (error, response, body) {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
-
-
 request('http://api.mapbox.com/geocoding/v5/mapbox.places/address=8837+Belair+Road.json?access_token=pk.eyJ1IjoidHJldm9yc3BlY2h0IiwiYSI6ImNqNDdjZ2hvNjAxYWkzMmp0YW90bWhsZmwifQ.0d4-iKYzF8sy7YFpzVOKUA', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  if(error){
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+
+  const array = {features:
+                  [{
+                    relevance: 1
+                  },
+                    {
+                      relevance: 2
+                    },
+                    {
+                      relevance: 3
+                    }
+                  ]};
+
+  const array123 = [1, 2, 3];
+
+
+// console.log(data);
+
+  // for (i = 0; i < data.features.length; i++) {
+  //   console.log(data.features[i].relevance);
+  //   console.log(data.features[i].place_name);
+  // }
+
+  array123.forEach(function(element) {
+  console.log(element);
+  });
+
 });
